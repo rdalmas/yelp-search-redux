ddc6c14003a07891a331e78d7e94137559e3962f
Rodrigo Dalmas <rodrigo.dalmas@penguinformula.com>
2020-04-23 11:51:07 +0100

    Fixing razzle config to deploy to heroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,12 +1,15 @@
 'use strict';
+
 const path = require('path');
+const razzleHeroku = require('razzle-heroku');
 
 module.exports = {
-  modify(config) {
-    require("razzle-heroku")
+  modify(config, { target, dev }, webpack) {
     const appConfig = config;
     // CHANGING ENTRY POINT FOR CLIENT
     appConfig.entry.client = path.resolve(__dirname, './src/client/client.js')
-    return appConfig;
+    config = razzleHeroku(appConfig, { target, dev }, webpack);
+
+    return config;
   },
 };
