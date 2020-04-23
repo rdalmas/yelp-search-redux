9edeea9691f405e3e3c235154907414655b23cfc
Rodrigo Dalmas <rodrigo.dalmas@penguinformula.com>
2020-04-23 02:28:36 +0100

    Changing razzle config

--- razzle.config.js
+++ razzle.config.js
@@ -2,8 +2,8 @@
 const path = require('path');
 
 module.exports = {
-  modify: require("razzle-heroku"),
   modify(config) {
+    require("razzle-heroku")
     const appConfig = config;
     // CHANGING ENTRY POINT FOR CLIENT
     appConfig.entry.client = path.resolve(__dirname, './src/client/client.js')
