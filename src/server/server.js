00531a830ad8896c981228a246286013e5e52f13
Rodrigo Dalmas <rodrigo.dalmas@penguinformula.com>
2020-04-24 11:00:14 +0100

    Adding cors origin configuration

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ import serverRender from "./serverRender";
 import routes from "./routes";
 
 const server = express();
-server.use(cors());
+server.use(cors({ origin: process.env.RAZZLE_ORIGIN }));
 
 // APIs
 server.use("/api", routes);
