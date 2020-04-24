826337902d30a6d0fbb1afbaa906f12b810b9c30
Rodrigo Dalmas <rodrigodalmas@gmail.com>
2020-04-24 12:02:03 +0100

    Removing helmet

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,12 @@
 
 import express from 'express';
 import cors from 'cors';
-import helmet from 'helmet';
 
 import serverRender from "./serverRender";
 import routes from "./routes";
 
 const server = express();
 server.use(cors({ origin: process.env.RAZZLE_ORIGIN }));
-server.use(helmet());
 
 // APIs
 server.use("/api", routes);
